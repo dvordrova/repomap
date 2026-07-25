b438a6b5ffb5ccb00dfd5dfabc9332f041b6909a
Dmitry Bozhko <meridos.programs@yandex.ru>
2026-07-25 03:49:01 +0300

    refactor: extract authorized workspace snapshot

---
+++ internal/workspacesnapshot/dependency_test.go
@@ -0,0 +1,51 @@
+package workspacesnapshot
+
+import (
+	"go/parser"
+	"go/token"
+	"io/fs"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestProductionImportsStayPresentationNeutral(t *testing.T) {
+	t.Parallel()
+
+	_, currentFile, _, ok := runtime.Caller(0)
+	if !ok {
+		t.Fatal("could not locate workspacesnapshot package")
+	}
+	directory := filepath.Dir(currentFile)
+	packages, err := parser.ParseDir(
+		token.NewFileSet(),
+		directory,
+		func(info fs.FileInfo) bool {
+			return !strings.HasSuffix(info.Name(), "_test.go")
+		},
+		parser.ImportsOnly,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	allowed := map[string]bool{
+		"github.com/dvordrova/repomap/internal/freshness":     true,
+		"github.com/dvordrova/repomap/internal/sourcecatalog": true,
+	}
+	for _, parsedPackage := range packages {
+		for filename, file := range parsedPackage.Files {
+			for _, imported := range file.Imports {
+				importPath, err := strconv.Unquote(imported.Path.Value)
+				if err != nil {
+					t.Fatalf("%s: malformed import: %v", filename, err)
+				}
+				if strings.HasPrefix(importPath, "github.com/dvordrova/repomap/internal/") &&
+					!allowed[importPath] {
+					t.Fatalf("%s imports disallowed internal dependency %q", filename, importPath)
+				}
+			}
+		}
+	}
+}
